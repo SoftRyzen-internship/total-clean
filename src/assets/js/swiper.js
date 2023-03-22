41f5b2acf1c0fc800a1497bc18cee9123d003734
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2023-03-22 09:35:58 +0200

    TC-7: init reviews

---
+++ src/assets/js/swiper.js
@@ -0,0 +1,60 @@
+import Swiper, { Pagination } from 'swiper';
+
+const swiper = new Swiper('.swiper', {
+  modules: [Pagination],
+
+  // autoplay: {
+  //   delay: 3000,
+  // },
+
+  freeMode: true,
+  speed: 800,
+  loop: true,
+
+  // effect: 'coverflow',
+  // preloadImages: false,
+
+  // coverflowEffect: {
+  //   rotate: 20,
+  //   stretch: 50,
+  //   slideShadows: true,
+  // },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+    dynamicMainBullets: 1,
+  },
+
+  slideToClickedSlide: true,
+  watchOverflow: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 24,
+      slidesPerGroup: 1,
+    },
+    768: {
+      slidesPerView: 2.4,
+      spaceBetween: 32,
+      slidesPerGroup: 2,
+    },
+    1280: {
+      slidesPerView: 4,
+      spaceBetween: 32,
+      slidesPerGroup: 4,
+    },
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+
+  mousewheel: {
+    sensitivity: true,
+    eventsTarget: '.swiper',
+  },
+});
